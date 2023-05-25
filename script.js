bf89ba9b4798f1c77a6164cc74ad2160aad653f4
David López Castillo <david.lopez.castillo@hotmail.com>
2023-05-24 20:40:17 -0600

    Se agrego la función para desencriptar el texto

--- script.js
+++ script.js
@@ -20,6 +20,24 @@ function encriptartexto(){
     
     mostrartexto.innerHTML=textosalida;
 
-    console.log(textosalida);
+}
+
+function desencriptartexto(){
+
+    textoentrada=document.getElementById("texto-ingresado").value;
+
+    textosalida=textoentrada.replaceAll("enter","e");
+    textosalida=textosalida.replaceAll("imes","i");
+    textosalida=textosalida.replaceAll("ai","a");
+    textosalida=textosalida.replaceAll("ober","o");
+    textosalida=textosalida.replaceAll("ufat","u");
+
+    document.getElementById("salida-texto").style.display="none";
+
+    document.getElementById("mostrar").style.display="block";
+
+    var mostrartexto=document.getElementById("mostrar-resultado");
+    
+    mostrartexto.innerHTML=textosalida;
 
 }
